1a9be812d4cca29218985655e80e43c4a5122e99
kMed <kenta.medina@protonmail.com>
2024-01-08 16:16:09 +0900

    (bugfix) fixed compilation error
    - fixed compilation error

--- Assets/Scripts/HealthManager.cs
+++ Assets/Scripts/HealthManager.cs
@@ -8,7 +8,6 @@ public class HealthManager : MonoBehaviour
     [SerializeField] private float scoreWhenKilled = 100f;
     [SerializeField] private GameObject deathPrefab; // sound + particle system + smoke (to hide disappearance of model)
     // it may be benificial to have a grace period to obscure the model before it is destroyed. deactivate movement components, destroy ~0.2f later.
-    [SerializeField] private GameObject deathPrefab;
 
     [Space]
     [Header("Data References")]
