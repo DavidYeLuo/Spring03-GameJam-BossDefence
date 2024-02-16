54c7fca82a4b8c76c555246151885e48539326e3
kMed <kenta.medina@protonmail.com>
2024-02-16 10:55:44 +0900

    basic dash
    - left shift to perform a short dash
    - adjustable dash duration and cooldown.
    - using animation curve may be desirable to customize speed throughout the dash. Also give the option to perform a teleport dash. A trail or particle effect may be useful to emphasize movement.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,7 +7,12 @@ public class PlayerController : MonoBehaviour
 {
     [Tooltip("horizontal speed of the player in m/s")]
     [SerializeField] private float speed = 2f;
-
+    [SerializeField] private float dashCooldown = 5f;
+    [SerializeField] private float dashDuration = 0.5f;
+    private bool isDashing = false;
+    private float currDashCooldown = 0f;
+    private float currDashDuration = 0f;
+    private Vector3 movementDelta;
     // private statuses
     
 
@@ -19,7 +25,30 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 movementDelta = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
-        transform.Translate(movementDelta*Time.deltaTime*speed);
+        if(currDashCooldown > 0f)
+        {
+            currDashCooldown = Mathf.Clamp(currDashCooldown - Time.deltaTime, 0f, dashCooldown);
+        }
+        else if(Input.GetKeyDown(KeyCode.LeftShift) && !isDashing)
+        {
+            isDashing = true;
+            currDashDuration = dashDuration;
+            currDashCooldown = dashCooldown;
+        }
+        if(isDashing)
+        {
+            transform.Translate(movementDelta*Time.deltaTime*speed*5f);
+            currDashDuration = Mathf.Clamp(currDashDuration - Time.deltaTime, 0f, dashDuration);
+            if(currDashDuration == 0f)
+            {
+                isDashing = false;
+            }
+        }
+        else
+        {
+            movementDelta = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+            movementDelta = movementDelta.normalized;
+            transform.Translate(movementDelta*Time.deltaTime*speed);
+        }
     }
 }
