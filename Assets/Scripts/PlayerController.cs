a07eb66592aca9180435e3a856e9ebe5c9f125db
kMed <kenta.medina@protonmail.com>
2024-02-18 02:41:39 +0900

    player dash
    - player dash trail
    - ability to store an arbitrary amount of dashes which recharge over time.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,11 +7,16 @@ public class PlayerController : MonoBehaviour
 {
     [Tooltip("horizontal speed of the player in m/s")]
     [SerializeField] private float speed = 2f;
-    [SerializeField] private float dashCooldown = 5f;
+    [SerializeField] private float dashCooldown = 0.2f;
+    [SerializeField] private float dashReload = 1f;
     [SerializeField] private float dashDuration = 0.5f;
+    [SerializeField] private TrailRenderer myTr;
+    [SerializeField] private int maxStoredDashes = 1;
     private bool isDashing = false;
     private float currDashCooldown = 0f;
+    private float currDashReload = 0f;
     private float currDashDuration = 0f;
+    private int currStoredDashes = 1;
     private Vector3 movementDelta;
     // private statuses
     
@@ -19,22 +24,34 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        currStoredDashes = maxStoredDashes;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(currStoredDashes<maxStoredDashes)
+        {
+            currDashReload -= Time.deltaTime;
+            if(currDashReload <= 0f)
+            {
+                currDashReload = dashReload;
+                currStoredDashes++;
+            }
+        }
+
         if(currDashCooldown > 0f)
         {
             currDashCooldown = Mathf.Clamp(currDashCooldown - Time.deltaTime, 0f, dashCooldown);
         }
-        else if(Input.GetKeyDown(KeyCode.LeftShift) && !isDashing)
+        else if(Input.GetKeyDown(KeyCode.LeftShift) && !isDashing && currStoredDashes > 0f)
         {
             isDashing = true;
             currDashDuration = dashDuration;
             currDashCooldown = dashCooldown;
+            currStoredDashes--;
         }
+
         if(isDashing)
         {
             transform.Translate(movementDelta*Time.deltaTime*speed*5f);
@@ -50,5 +67,6 @@ public class PlayerController : MonoBehaviour
             movementDelta = movementDelta.normalized;
             transform.Translate(movementDelta*Time.deltaTime*speed);
         }
+        myTr.emitting = isDashing;
     }
 }
